cbc7f5cf7691007b669cfaae6f25fcad448c7173
Kristoffer Krueger <104184753+blazeon1313@users.noreply.github.com>
2022-07-11 02:48:23 -0600

    calculatorJS

---
+++ week4/calculatorJS/calculator.js
@@ -0,0 +1,15 @@
+// calulator JS page
+
+function dis(val){
+    document.getElementById("result").value+=val
+}
+  
+function solve(){
+    let a = document.getElementById("result").value
+    let b = eval(a)
+    document.getElementById("result").value = b
+}
+  
+function clr(){
+    document.getElementById("result").value = ""
+}
