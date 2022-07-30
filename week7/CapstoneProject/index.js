5289ae46ca84d6f8a8e9d73ab33c06ed850bacc7
Kristoffer Krueger <104184753+blazeon1313@users.noreply.github.com>
2022-07-30 00:22:46 -0600

    capstone to do list

---
+++ week7/CapstoneProject/index.js
@@ -0,0 +1,50 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Delete");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+} 
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var inputValue2 = document.getElementById("input2").value;
+  var t = document.createTextNode(inputValue + "~" + "\n" + 'Chore to do: ' + inputValue2);
+  li.appendChild(t);
+
+  document.getElementById("chores").appendChild(li);
+  document.getElementById("input").value = "";
+  document.getElementById("input2").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Delete");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
